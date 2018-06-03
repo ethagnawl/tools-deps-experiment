96cc3d754040d0f583819ada881b596c89850664
Peter Doherty <pdoherty926@gmail.com>
2018-06-03 18:01:31 -0400

    add github dependency to deps.edn

--- src/hello.clj
+++ src/hello.clj
@@ -1,10 +1,24 @@
 (ns hello
-  (:require [com.rpl.specter :as specter]))
+  (:require [clojure.algo.monads :as monad]
+            [com.rpl.specter :as specter]))
 
 (defn inc-last-odd [xs]
+  "Increment last odd number. Otherwise, noop if none exist."
   (specter/transform [(specter/filterer odd?) specter/LAST] inc xs))
 
+(defn maybe-square-last-even-int [xs]
+  "Square last int if even. Otherwise, fail early and return nil."
+  (monad/domonad monad/maybe-m
+    [last-even (-> xs
+                 reverse
+                 first
+                 (#(if (even? %) % nil)))]
+    (* last-even last-even)))
+
 (defn -main []
-  (let [input [1 2 3]
-        result (inc-last-odd input)]
-    (println (str "The result of calling inc-last-odd with " input " is " result))))
+  (let [inc-last-odd-input [1 2 3]
+        inc-last-odd-result (inc-last-odd inc-last-odd-input)
+        square-last-even-input [1 2]
+        square-last-even-result (maybe-square-last-even-int square-last-even-input)]
+    (println (str "The result of calling inc-last-odd with " inc-last-odd-input " is " inc-last-odd-result))
+    (println (str "The result of calling maybe-square-last-even-int with " square-last-even-input " is " square-last-even-result))))
--- test/hello/test.clj
+++ test/hello/test.clj
@@ -2,8 +2,25 @@
   (:use midje.sweet)
   (:require [hello :as hello]))
 
-(facts "facts"
-  (fact "fact"
-        (let [expected [1 2 4]
-              actual (hello/inc-last-odd [1 2 3])]
-        expected => actual)))
+(facts "hello facts"
+
+  (facts "inc-last-odd facts"
+    (fact "inc last odd int"
+          (let [expected [1 2 4]
+                actual (hello/inc-last-odd [1 2 3])]
+          expected => actual))
+    (fact "noop if there are no odds"
+          (let [expected [2 4]
+                actual (hello/inc-last-odd [2 4])]
+          expected => actual)))
+
+  (facts "maybe-square-last-even-int facts"
+    (fact "square last even int"
+          (let [expected 4
+                actual (hello/maybe-square-last-even-int [1 2])]
+          expected => actual))
+    (fact "fail early and return nil when last int isn't even"
+          (let [expected nil
+                actual (hello/maybe-square-last-even-int [1])]
+          expected => actual))))
+
